3e422b5512b25e2dc3e573b46452ebf2cc2c0bc9
Jerred Shepherd <github@sjer.red>
2025-01-05 14:52:44 -0800

    fix: migration

---
+++ packages/backend/prisma/migrations/20250105225221_remove_unique/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Account_serverId_puuid_key";
+
+-- DropIndex
+DROP INDEX "Account_serverId_summonerId_key";
+
+-- DropIndex
+DROP INDEX "Player_serverId_discordId_key";
+
+-- DropIndex
+DROP INDEX "Subscription_serverId_playerId_channelId_key";
