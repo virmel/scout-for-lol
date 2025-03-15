18b0b260671808156b560de0e6674b4351a93714
Jerred Shepherd <github@sjer.red>
2025-03-15 10:26:18 -0700

    fix: deps and whatnot

--- packages/report/src/assets/index.ts
+++ packages/report/src/assets/index.ts
@@ -11,177 +11,177 @@ export const font = {
 export const fonts: Font[] = [
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Light.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 300,
     style: "normal",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-LightItalic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 300,
     style: "italic",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Regular.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 400,
     style: "normal",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Italic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 400,
     style: "italic",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Medium.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 500,
     style: "normal",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-MediumItalic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 500,
     style: "italic",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Bold.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 700,
     style: "normal",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-BoldItalic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 700,
     style: "italic",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-Heavy.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 800,
     style: "normal",
   },
   {
     name: font.title,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/BeaufortForLoL-TTF/BeaufortforLOL-HeavyItalic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 800,
     style: "italic",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_Regular.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 400,
     style: "normal",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_Regular_Italic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 400,
     style: "italic",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_SemiBold.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 500,
     style: "normal",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_SemiBold_Italic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 500,
     style: "italic",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_Bold.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 700,
     style: "normal",
   },
   {
     name: font.body,
-    data: await Deno.readFile(
+    data: (await Deno.readFile(
       new URL(
         `${fontPath}/Spiegel-TTF/Spiegel_TT_Bold_Italic.ttf`,
         import.meta.url,
       ),
-    ),
+    )).buffer,
     weight: 700,
     style: "italic",
   },
